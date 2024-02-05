9c0f1535e37623e2dc6effc0e1bf9e33e200e459
4lch4 <git@4lch4.email>
2024-02-05 11:42:44 -0600

    refactor: moved main code to index.ts & added some logging

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,34 @@
-import { run } from './main.js'
+import * as core from '@actions/core'
+import { context } from '@actions/github'
+import { GitHubUtil } from './GitHubUtil.js'
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-run()
+try {
+  /**
+   * A string like in the format of `<number> <unit>` like "30 days" that determines how long a
+   * branch must go without activity to be considered stale.
+   */
+  const staleBranchAge: string = core.getInput('stale-branch-age')
+
+  /** The personal access token used for authenticating with the GitHub API. */
+  const token: string = core.getInput('github-token')
+
+  const gh = new GitHubUtil(token)
+
+  const branchesAndCommits = await gh.getBranchesAndLatestCommit(
+    context.repo.owner,
+    context.repo.repo,
+  )
+
+  core.setOutput('branches-and-commits', JSON.stringify(branchesAndCommits))
+  core.setOutput('stale-branch-age', staleBranchAge)
+  core.setOutput('branches-count', branchesAndCommits?.length || 0)
+
+  core.info('Action completed successfully!')
+
+  core.info(`Stale Branch Age: ${staleBranchAge}`)
+  core.info(`Branches Count: ${branchesAndCommits?.length || 0}`)
+} catch (error) {
+  if (error instanceof Error) core.setFailed(error.message)
+
+  core.setFailed('An error occurred, check logs for more information.')
+}
