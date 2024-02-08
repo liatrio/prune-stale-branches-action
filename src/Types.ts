4066b074a1c39334722db9589d5470b3a3a127ae
4lch4 <git@4lch4.email>
2024-02-08 11:44:03 -0600

    feat: cleaned up & added some types

--- src/Types.ts
+++ src/Types.ts
@@ -1,10 +1,12 @@
 import { context } from '@actions/github'
 import { type Endpoints } from '@octokit/types'
-import { Dayjs } from 'dayjs'
+import { Dayjs, ManipulateType } from 'dayjs'
 
-export type CommitResponse =
-  Endpoints['GET /repos/{owner}/{repo}/commits/{ref}']['response']['data']
-
+/**
+ * An object containing the data available as input from the GitHub Action.
+ *
+ * _NOTE: This can likely be deleted but I'm keeping it here just in case it's needed later._
+ */
 export type ActionsInput = {
   /**
    * The date that is the cutoff for when a branch is considered stale. Any branch that has not had
@@ -23,6 +25,7 @@ export type ActionsInput = {
   token: string
 }
 
+/** An object containing the relevant data for a branch and its last commit. */
 export type BranchAndCommit = {
   /** The details available for a branch. */
   branch: Endpoints['GET /repos/{owner}/{repo}/branches']['response']['data'][number]
@@ -31,16 +34,33 @@ export type BranchAndCommit = {
   commit: Endpoints['GET /repos/{owner}/{repo}/commits/{ref}']['response']['data']
 }
 
+export type CommitResponse =
+  Endpoints['GET /repos/{owner}/{repo}/commits/{ref}']['response']['data']
+
+/** An object containing the data needed to create a `Dayjs` instance. */
+export type DateInputValues = {
+  /** The number of units to subtract or add to the current date. */
+  number: number
+
+  /** The unit of time to subtract or add to the current date. */
+  unit: ManipulateType
+}
+
+/** An object containing the relevant data for a branch that has been flagged for deletion. */
+export type FlaggedBranch = {
+  /** Details about the repo where the branch lives. */
+  repo: typeof context.repo
+
+  /** The name of the branch that has been flagged for deletion. */
+  branchName: string
+
+  /** An object containing data about the last commit made to the flagged branch. */
+  lastCommit: CommitResponse
+}
+
 /** The response from the GitHub API when fetching issues. */
 export type IssuesResponse =
   Endpoints['GET /repos/{owner}/{repo}/issues']['response']['data'][number]
 
 /** The response from the GitHub API when fetching repositories. */
 export type ReposResponse = Endpoints['GET /orgs/{org}/repos']['response']['data'][number]
-
-export type FlaggedBranch = {
-  repo: typeof context.repo
-  branchName: string
-  // lastCommitDate: Dayjs
-  lastCommit: CommitResponse
-}
